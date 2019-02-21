8c499731183b1ffaab3d5f6a5f68c264cf9d7af5
Dumouchel <dumouchel@dessia.tech>
2019-02-21 01:02:47 +0100

    MAJ du parametrage de la roue

--- ParkingLock.py
+++ ParkingLock.py
@@ -10,6 +10,7 @@ import numpy as npy
 import matplotlib.pyplot as plt
 
 import volmdlr as vm
+import volmdlr.primitives2D as primitives2D
 
 class Pawl:
     """
@@ -83,68 +84,108 @@ class Pawl:
         sol2 = fsolve(fonct, 0, args=(CF, self.DF))
         F2  = R.Translation(vm.Point2D((self.DF/2.*npy.cos(npy.pi/2. + sol2), self.DF/2.*npy.sin(npy.pi/2. + sol2))))
         
-        
         Cr = vm.Circle2D(R, self.DF/2.)
         
         c=vm.CompositePrimitive2D([O, AB, BP, PM, DE, EC, DG, CF, R, Cr, F1, F2])
+        return c
+    
+    def Plot(self):
+        c = self.Geometry()
         c.MPLPlot(style='--ob')
         
 
     
 class Gear:
     
-    def __init__(self,Ox,Oy,Z,DI,DF,L8,L9,theta1,theta2,theta3,alphaJ,alphaR):
+    def __init__(self, Ox, Oy, Z, DI, DF, DE, 
+                 alpha1, alpha2, alpha3):
         self.Ox = Ox
         self.Oy = Oy
         self.Z = Z
         self.DI = DI
         self.DF = DF
-        self.L8 = L8
-        self.L9 = L9 #Distance JK
-        self.theta1 = theta1
-        self.theta2 = theta2 
-        self.theta3 = theta3 #(theta2+theta3)=(2*npy.pi/Z) ?
-        self.alphaJ = alphaJ
-        self.aplhaR = alphaR
-        
-        
+        self.DE = DE
+        self.alpha1 = alpha1
+        self.alpha2 = alpha2
+        self.alpha3 = alpha3
         
        
     def Geometry(self):
-        theta = 2*npy.pi/Z #theta=theta2 +theta3
         
-        R = O.Translation(vm.point2D((self.L8*npy.cos(alphaR),-self.L8*npy.sin(alphaR))))
+        pas = 2*npy.pi/self.Z
+        O = vm.Point2D((self.Ox, self.Oy))
+        A = O.Translation(vm.Point2D((0, self.DE/2.)))
+        Bp = O.Translation(vm.Point2D((0, self.DI/2.)))
+        B = Bp.Rotation(O, -self.alpha1, copy = True)
+        Bi = B.Rotation(O, -self.alpha2/2., copy = True)
+        C = B.Rotation(O, -self.alpha2, copy = True)
+        D = A.Rotation(O, -self.alpha1 - self.alpha2 - self.alpha3, copy = True)
+        Di = D.Rotation(O, (-pas + self.alpha1 + self.alpha2 + self.alpha3)/2., copy = True)
+        Ap = A.Rotation(O, -pas, copy = True)
         
-        K = R.Translation(vm.point2D((0,DF/2)))
-        H = K.Rotation(R,theta2)
-        M = K.Rotation(R,theta2+theta3)
-        a1 = vm.Arc2D(K,H,M)
+        dir1 = (B[1] - A[1])/(B[0] - A[0])
+        ord1 = B[1] - dir1*B[0]
+        def fonct(x, *data):
+            Ox, Oy, R, coeff_dir, ord_origine = data
+            f = (ord_origine + coeff_dir*x - Oy)**2 + (x - Ox)**2 - R**2
+            return f
+        x1 = fsolve(fonct, 0, args=(self.Ox, self.Oy, self.DF/2., dir1, ord1))
+        R1 = vm.Point2D((x1, ord1 + x1*dir1))
         
-        J1 = R.Translation(vm.Point2D((0,DI/2)))
-        J=J1.Rotation(R,(theta2-theta1)/2)
-        #est ce un probleme de ne pas controler directement la pente du créneau ? (on ne se sert pas de alphaJ)
-        #sinon on pourrait remplacer les deux lignes précédentes par:J=K.Translation(vm.point2D((-self.L9*npy.sin(alphaJ),-self.L9*npy.cos(alphaJ))))
-        #Dans ce cas il faudrait peut être paramétrer H de la même façon
-        I=J.Rotation(R,theta1)
+        dir2 = (D[1] - C[1])/(D[0] - C[0])
+        ord2 = C[1] - dir2*C[0]
+        x2 = fsolve(fonct, 1, args=(self.Ox, self.Oy, self.DF/2., dir2, ord2))
+        R2 = vm.Point2D((x2, ord2 + x2*dir2))
         
-        l1=primitives2D.RoundedLineSegments2D([K,J,I,H],{'''je ne sais pas trop quoi mettre ici'''})
+        li = [B, Bi, C, D, Di, Ap]
+        AB=vm.LineSegment2D(A, B)
+        BC = vm.Arc2D(B, Bi, C)
+        CD=vm.LineSegment2D(C, D)
         
-        L=[a1,l1]
-        for i in range(Z-1):
-            thetar=(i+1)*theta
-            L.append(a1.Rotation(R,thetar,True))
-            L.append(l1.Rotation(R,thetar,True))
+        list_seg = []
+        Am = A
+        for z in range(self.Z):
+            li_point = []
+            for pt in li:
+                li_point.append(pt.Rotation(O, -pas*z, copy=True))
+            AB = vm.LineSegment2D(Am, li_point[0])
+            BC = vm.Arc2D(li_point[0], li_point[1], li_point[2])
+            CD=vm.LineSegment2D(li_point[2], li_point[3])
+            DA = vm.Arc2D(li_point[3], li_point[4], li_point[5])
+            list_seg.extend([AB, BC, CD, DA])
+            Am = li_point[5]
+                
+        c=vm.CompositePrimitive2D(list_seg)
+        return c
+    
+    def Plot(self):
+        c = self.Geometry()
+        c.MPLPlot(style='--ob')
 
-        c1=vm.Contour2D(L)
-        c1.MPLPlot()
-
+class ParkingLock:
+    def __init__(self, pawl, gear, pos_pawl, alpha_pawl, pos_gear, alpha_gear):
+        
+        self.pawl = pawl
+        self.gear = gear
+        self.pos_pawl = pos_pawl
+        self.alpha_pawl = alpha_pawl
+        self.pos_gear = pos_gear
+        self.alpha_gear = alpha_gear
+    
+#    def Geometry(self):
+        
+        
         
 P1 = Pawl(Ox = 0, Oy = 0, alpha0 = npy.pi/4., LA = 1,
           alphaA = npy.pi/4., L1 = 0.1, alphaB = npy.pi/4., L2 = 0.1,
           alphaP = npy.pi/6., L5 = 0.1, alphaD = npy.pi/6., LE = 0.8, 
           alpha1 = npy.pi/4. - npy.pi/8., L3 = 0.05, L4 = 0.05, alphaE = npy.pi/8., L6 = 0.1, 
           alphaC = npy.pi/6., L7 = 0.1, DF = 1, DI = 0.9)
-P1.Geometry()
+P1.Plot()
+
+G1 = Gear(Ox = 0, Oy = 0, Z = 10, DI = 0.1, DF = 0.11, DE = 0.12, 
+          alpha1 = 0.1, alpha2 = 0.3, alpha3 = 0.1)
+G1.Plot()
 
 #G1 = Gear(teeth_number = 12, pitch_diameter = 0.07, point_o = [0, 0], 
 #          external_diameter = 0.08, internal_diameter = 0.06)
